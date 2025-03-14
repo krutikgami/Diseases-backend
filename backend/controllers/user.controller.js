4121563475f98c8eb3dff68883fed1819165e0b1
Krutik Gami <krutikgami354@gmail.com>
2025-03-14 21:47:18 +0530

    minor bug fixed for the user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,55 +2,63 @@ import { User } from "../models/user.model.js";
 
 
 const registerUser = async(req,res) => {
-    const {name,email,mobile_no,district,password,confirmPassword} = req.body;
-    if(!name || !email || !mobile_no || !district || !password || !confirmPassword){
-        return res.status(400).json({message:"All fields are required"});
-    }
-    if(password !== confirmPassword){
-        return res.status(400).json({message:"Confirm Password do not match"});
-    }
-    const user = await User.findOne({
-        email
-    })
-    if(user){
-        return res.status(400).json({message:"User already exists"});
-    }
-    const newUser = new User({
-        name,
-        email,
-        mobile_no,
-        district,
-        password,
-        confirmPassword
-    })
-    await newUser.save();
-    const createdUser = await User.findById(newUser._id).select("-password -confirmPassword");
-    return res.status(201).json({data:createdUser,message:"User registered successfully"});  
-    
+   try {
+     const {name,email,mobile_no,district,password,confirmPassword} = req.body;
+     if(!name || !email || !mobile_no || !district || !password || !confirmPassword){
+         return res.status(400).json({message:"All fields are required"});
+     }
+     if(password !== confirmPassword){
+         return res.status(400).json({message:"Confirm Password do not match"});
+     }
+     const user = await User.findOne({
+         email
+     })
+     if(user){
+         return res.status(400).json({message:"User already exists"});
+     }
+     const newUser = new User({
+         name,
+         email,
+         mobile_no,
+         district,
+         password,
+         confirmPassword
+     })
+     await newUser.save();
+     const createdUser = await User.findById(newUser._id).select("-password -confirmPassword");
+     return res.status(201).json({data:createdUser,message:"User registered successfully"});  
+     
+   } catch (error) {
+        return res.status(500).json({message:"Internal Server Error"});
+   }
 }
 
 const loginUser = async(req,res) =>{
-    const {email,password} = req.body;
-
-    if(!email || !password || email == "" || password =="" ){
-        return res.status(404).json({message:"All fields are required"});
-    }
-
-    const user = await User.findOne({
-        email
-    });
-
-    if(!user){
-        return res.status(404).json({message:"User not Exist"});
-    }
-    const passwordMatch  = await user.matchPassword(password);
-    if(!passwordMatch){
-        return res.status(404).json({message:"Invalid Credentials"});
-    }
+    try {
+        const {email,password} = req.body;
     
-    const createdUser = await User.findById(user._id).select("-password -confirmPassword");
+        if(!email || !password || email == "" || password =="" ){
+            return res.status(404).json({message:"All fields are required"});
+        }
     
-    return res.status(200).json({data: createdUser,message:"User LoggedIn successfully"});
+        const user = await User.findOne({
+            email
+        });
+    
+        if(!user){
+            return res.status(404).json({message:"User not Exist"});
+        }
+        const passwordMatch  = await user.matchPassword(password);
+        if(!passwordMatch){
+            return res.status(404).json({message:"Invalid Credentials"});
+        }
+        
+        const createdUser = await User.findById(user._id).select("-password -confirmPassword");
+        
+        return res.status(200).json({data: createdUser,message:"User LoggedIn successfully"});
+    } catch (error) {
+        return res.status(500).json({message:"Internal Server Error"});
+    }
 }
 
 const getUser = async(req,res)=>{
