4829c9f33eae237817a03a6eac838342ec17de71
Krutik Gami <krutikgami354@gmail.com>
2025-03-15 13:21:10 +0530

    role name convention changed

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     },
     role:{
         type:String,
-        default:"districtuser"
+        default:"district-head"
     },
     password:{
         type:String,
