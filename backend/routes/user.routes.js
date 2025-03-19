46cbb47a7cec36847c58d84606b6b5cabdbe2885
Krutik Gami <krutikgami354@gmail.com>
2025-03-19 23:54:23 +0530

    getUsers api from get changes to post with auth

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { loginUser,registerUser,getUser,districtHeadDelete } from "../controllers/user.controller.js"
 import express from "express";
+import { protectStateHead } from "../middleware/state_head.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup",registerUser);
 router.post("/login",loginUser);
-router.get("/users",getUser);
+router.post("/users",protectStateHead,getUser);
 router.delete("/district-head/delete",districtHeadDelete);
 
 export default router;
